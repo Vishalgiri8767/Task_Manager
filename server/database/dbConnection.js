5f56984964de359f560baeadf1f2284159f86869
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-05-10 12:52:25 +0530

    inital commit

---
+++ server/database/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  const MONGO_URI = 'mongodb+srv://vishal:giri@cluster0.sotiin8.mongodb.net/'
+  mongoose
+    .connect(MONGO_URI, {
+      dbName: "TASK_MANAGEMENT_DB",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database! : ${err}`);
+    });
+};
